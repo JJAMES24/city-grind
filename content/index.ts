a8a60f8d1bacd44e91a4c93b8df75e503c5c05f0
Jarod <jjamesprojects@hotmail.com>
2023-12-20 12:42:16 -0400

    link added to contact form closes #19

--- content/index.ts
+++ content/index.ts
@@ -5,7 +5,7 @@ export const NAV_LINKS = [
     { href: '/', key: 'home', label: 'HOME' },
     { href: '/', key: 'about_us', label: 'ABOUT US' },
     { href: '/', key: 'menu', label: 'MENU' },
-    { href: '/', key: 'contact_us', label: 'CONTACT US' },
+    { href: 'https://us12.list-manage.com/contact-form?u=48d1b8450dfb67c992fec444c&form_id=efad744df20348a72427317d236831e6', key: 'contact_us', label: 'CONTACT US' },
   ];
 
   //HERO CONTENT 
